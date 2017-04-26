7efbda62e1e518bb3c395667f4313bbf06432dec
Aman Vishnani <aman62@gmail.com>
2017-04-27 01:10:51 +0530

    Bug fixes

--- database/abc.php
+++ database/abc.php
@@ -1,17 +1,32 @@
 <?php
-$con=mysql_connect('localhost','root','');
+$con=mysql_connect('localhost','root','aman');
 if(!$con)
 {
 	die('could not connect:'.mysql_error());
 }
-echo "Connection establised";
-$db=mysql_select_db("mgm",$con);
+echo "Connection establised</br>";
+$db=mysql_select_db("test",$con) or die('DB does not exist');
+
+$sql_create = "CREATE TABLE IF NOT EXISTS stud (name varchar(25),class varchar(6),roll varchar(3))";
+
+mysql_query($sql_create) or die("Error Creating DB".mysql_error());
+
 $name = $_GET["n1"];
 $class = $_GET["n2"];
 $rollno = $_GET["n3"];
 $query ="Insert into stud values('$name','$class','$rollno');";
 //$query ="select * from stud;";
-mysql_query($query);
+mysql_query($query) or die('Error inserting data');
+
+$rows = mysql_query("SELECT * FROM stud;");
+echo "<pre>";
+
+while($row = mysql_fetch_array($rows))
+{
+	print_r($row);
+}
+echo "</pre>";
+
 mysql_close();
 CONST a="</br>";
 echo "Name:".$_GET["n1"].a;
